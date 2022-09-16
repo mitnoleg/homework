afa72d38984461aaaf56a6302b5561fe1c91bc84
mitnoleg <user@example.com>
2022-09-17 01:03:38 +0300

    done

--- src/module2/lesson8/homework.java
+++ src/module2/lesson8/homework.java
@@ -1,7 +1,12 @@
 package module2.lesson8;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class homework {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, InvocationTargetException,
+            IllegalAccessException {
         //1. Создать аннотацию GetMetaData, которая может быть над атрибутом, методом, классом
         //2. Создать класс UserMeta, с атрибутом String name, Integer count, методом GetSuperName(String prefix)
         //3. Создать обработчик аннотации GetMetaData, так чтобы:
@@ -10,7 +15,28 @@ public class homework {
         //3.3 При нахождении @GetMetaData над методом, печатаем сколько времени выполнялся метод. - отдельный метод
         //4. Создаем класс Runner, запускаем
         UserMeta userMeta = new UserMeta("Ilya", 20);
-        userMeta.GetSuperName("Mr.");
+//        userMeta.GetSuperName("Mr.");
+
+        for (Method declaredMethod : UserMeta.class.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(GetMetaData.class)) {
+               long start = System.currentTimeMillis();
+               declaredMethod.invoke(userMeta,"Mr. ");
+               long end = System.currentTimeMillis();
+               System.out.println(end - start);
+           }
+        }
+        for (Field declaredField : userMeta.getClass().getDeclaredFields()) {
+            declaredField.setAccessible(true);
+            if (declaredField.isAnnotationPresent(GetMetaData.class)) {
+                System.out.println(declaredField.get(userMeta));
+            }
+        }
+        for (Field declaredField : userMeta.getClass().getDeclaredFields()) {
+            declaredField.setAccessible(true);
+            if (declaredField.isAnnotationPresent(GetMetaData.class)) {
+                System.out.println(declaredField.get(userMeta));
+            }
+        }
     }
 
 
